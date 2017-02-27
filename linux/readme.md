d3d603dc2537f5f63f08efcd2d5ef769586fdd92
Nícolas 'eowfenth' Deçordi <eowfenth@gmail.com>
2017-02-26 21:50:23 -0300

    Added spotify script and zsh installation initial script

---
+++ linux/readme.md
@@ -0,0 +1,11 @@
+
+
+Troubleshooting
+
+#### 1. Permission denied
+
+Change permissions of the file you want to run.
+
+'''BASH
+chmod 775 file
+'''
---
+++ linux/scripts/install-zsh.sh
@@ -0,0 +1,14 @@
+# Check if zsh is not active
+
+# nor installed
+
+# installs it
+
+# make it default
+
+    chsh -s $(which zsh)
+    
+# alert it won't work until log off, ask for log off
+
+# log off
+
