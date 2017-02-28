4a77101acc8642d44a53a5faf628827e229800fe
Nícolas 'eowfenth' Deçordi <eowfenth@gmail.com>
2017-02-28 19:55:41 -0300

    Added installation script of GNOME extensions

---
+++ linux/scripts/install-gnome-extensions.sh
@@ -0,0 +1,5 @@
+# 1. Install Dynamic Panel Transparency
+
+cd ~/packages/extensions/
+git clone https://github.com/rockon999/dynamic-panel-transparency.git && cd dynamic-panel-transparency/
+cp -R ./ ~/.local/share/gnome-shell/extensions/
